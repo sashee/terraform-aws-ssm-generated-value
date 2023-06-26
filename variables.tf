636362527d73fd43af6a2b1fb279fa5e76c7f013
sashee <tamas.sallai@advancedweb.hu>
2023-06-26 11:57:48 +0200

    Update docs

--- variables.tf
+++ variables.tf
@@ -1,7 +1,9 @@
 variable "parameter_name" {
   type = string
+	description = "The SSM parameter's name"
 }
 
 variable "code" {
   type = string
+	description = "A Javascript source that exports a generate() function"
 }
